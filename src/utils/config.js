1f4408cae698526675a0a777c2fa4d7506c64893
Bharat <Bharat@softelevation.com>
2022-06-28 18:18:43 +0530

    tracking feature implemented

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const config = {
   // Api_Url: 'http://3.235.91.25:3000',
-  Api_Url: 'https://1bc8-112-196-35-18.in.ngrok.io',
+  Api_Url: 'https://44d7-112-196-35-18.in.ngrok.io',
 };
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,32 +1,6 @@
-// import io from 'socket.io-client';
-// let socket;
-// export const initiateSocket = (room) => {
-//   socket = io('http://3.235.91.25:3000');
-//   console.log('Connecting socket...');
-//   if (socket) {
-//     socket.emit('join');
-//   }
-//   socket.on('connect', (a) => {
-//     console.log('true', socket.connected); // true
-//   });
-// };
-// export const disconnectSocket = () => {
-//   console.log('Disconnecting socket...');
-//   if (socket) {
-//     socket.disconnect();
-//   }
-// };
-// export const subscribeToBooking = (cb) => {
-//   if (!socket) {
-//     return true;
-//   }
-//   socket.on('refresh_feed', (msg) => {
-//     console.log('Websocket event received!');
-//     return cb(null, msg);
-//   });
-// };
-// export const sendMessage = (message) => {
-//   if (socket) {
-//     socket.emit('book_now', message);
-//   }
-// };
+import React from 'react';
+import socketio from 'socket.io-client';
+import {config} from './config';
+
+export const socket = socketio(config.Api_Url);
+export const SocketContext = React.createContext();
