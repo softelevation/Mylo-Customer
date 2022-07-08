f09565978ccaa378868da1188c2c398ed58b8fa4
Bharat <Bharat@softelevation.com>
2022-07-08 13:18:27 +0530

    changes

--- src/components/activityLoader.js
+++ src/components/activityLoader.js
@@ -3,51 +3,35 @@ import {StyleSheet, ActivityIndicator, View} from 'react-native';
 import {light} from './theme/colors';
 
 const styles = StyleSheet.create({
-  loader: {
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    zIndex: 1000,
+  },
+  mainContainer: {
+    borderRadius: 10,
+    shadowColor: 'black',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'absolute',
-    backgroundColor: 'rgb(255,255,255)',
-    top: 0,
-    right: 0,
-    left: 0,
-    bottom: 0,
-    zIndex: 100,
+    padding: 25,
+    shadowOffset: {width: 0, height: 2},
+    maxWidth: 150,
+    maxHeight: 150,
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
 
 const ActivityLoader = () => {
   return (
-    // <ActivityIndicator
-    //   size="large"
-    //   color={light.secondary}
-    //   style={styles.loader}
-    // />
-    <View
-      style={{
-        height: '100%',
-        width: '100%',
-        backgroundColor: 'rgba(0,0,0,0.3)',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        zIndex: 1000,
-      }}>
-      <View
-        style={{
-          borderRadius: 10,
-          shadowColor: 'black',
-          backgroundColor: '#fff',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: 25,
-          shadowOffset: {width: 0, height: 2},
-          maxWidth: 150,
-          maxHeight: 150,
-          shadowOpacity: 0.4,
-          shadowRadius: 4,
-          elevation: 5,
-        }}>
+    <View style={styles.container}>
+      <View style={styles.mainContainer}>
         <ActivityIndicator size={'large'} color={light.secondary} />
       </View>
     </View>
